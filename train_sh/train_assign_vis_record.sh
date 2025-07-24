647c60bea3def621707f5a6d9b447d39c312e0a5
NuayHL <liuhy.df.xin@gmail.com>
2025-07-25 02:05:20 +0900

    assign record

---
+++ train_sh/train_assign_vis_record.sh
@@ -0,0 +1,11 @@
+echo 'Starts!'
+echo 'yolov12s_assign_record'
+yolo detect train \
+    data=ultralytics/cfg/datasets/VisDrone.yaml \
+    model=mycfg/yolo12s_assign_visdrone.yaml \
+    epochs=150 \
+    batch=16 \
+    imgsz=640 \
+    device=0 \
+    save=True \
+    name=assign_record/v12s_vis > terminal_log/yolov12s_assign_record.log 2>&1
---
+++ train_sh/train_assign_voc_record.sh
@@ -0,0 +1,11 @@
+echo 'Starts!'
+echo 'yolov12s_assign_record'
+yolo detect train \
+    data=ultralytics/cfg/datasets/VOC.yaml \
+    model=mycfg/yolo12s_assign_voc.yaml \
+    epochs=150 \
+    batch=16 \
+    imgsz=640 \
+    device=0 \
+    save=True \
+    name=assign_record/v12s_voc > terminal_log/yolov12s_assign_record.log 2>&1
