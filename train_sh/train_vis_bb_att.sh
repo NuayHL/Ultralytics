e29c2be9b50c07032611feab7c579a94d14db64f
liuhaoyuan_exp1 <liuhy.df.xin@gmail.com>
2025-07-25 15:40:10 +0900

    fix the pretrained training scripts

--- train_sh/train_vis_bb_att.sh
+++ train_sh/train_vis_bb_att.sh
@@ -1,7 +1,7 @@
 EXP_PREFIX="visdrone"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 #LOG_DIR="terminal_log/terminal_log_${EXP_PREFIX}_${TIMESTAMP}"
-LOG_DIR="terminal_log/terminal_log_${EXP_PREFIX}_${TIMESTAMP}"
+LOG_DIR="terminal_log/terminal_log_${EXP_PREFIX}"
 if [ -d "$LOG_DIR" ]; then
     LOG_DIR="${LOG_DIR}_$(date +%s)"
 fi
@@ -14,7 +14,7 @@ echo "${EXP_NAME}"
 yolo detect train data=ultralytics/cfg/datasets/VisDrone.yaml \
                   model=cfg/yolo12s_cbb.yaml \
                   epochs=150 batch=16 imgsz=640 device=0 save=True \
-                  name="${EXP_PREFIX}/${EXP_NAME}" pretrained=yolov8m.pt \
+                  name="${EXP_PREFIX}/${EXP_NAME}" \
                   > "$LOG_DIR/${EXP_NAME}.log" 2>&1
 
 EXP_NAME="vis_v12s_bb_ca"
@@ -22,7 +22,7 @@ echo "${EXP_NAME}"
 yolo detect train data=ultralytics/cfg/datasets/VisDrone.yaml \
                   model=cfg/yolo12s_ca.yaml \
                   epochs=150 batch=16 imgsz=640 device=0 save=True \
-                  name="${EXP_PREFIX}/${EXP_NAME}" pretrained=yolov8m.pt \
+                  name="${EXP_PREFIX}/${EXP_NAME}" \
                   > "$LOG_DIR/${EXP_NAME}.log" 2>&1
 
 EXP_NAME="vis_v12s_bb_se"
@@ -30,6 +30,6 @@ echo "${EXP_NAME}"
 yolo detect train data=ultralytics/cfg/datasets/VisDrone.yaml \
                   model=cfg/yolo12s_se.yaml \
                   epochs=150 batch=16 imgsz=640 device=0 save=True \
-                  name="${EXP_PREFIX}/${EXP_NAME}" pretrained=yolov8m.pt \
+                  name="${EXP_PREFIX}/${EXP_NAME}" \
                   > "$LOG_DIR/${EXP_NAME}.log" 2>&1
 
--- train_sh/train_vis_bbc_att.sh
+++ train_sh/train_vis_bbc_att.sh
@@ -1,7 +1,7 @@
 EXP_PREFIX="visdrone"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 #LOG_DIR="terminal_log/terminal_log_${EXP_PREFIX}_${TIMESTAMP}"
-LOG_DIR="terminal_log/terminal_log_${EXP_PREFIX}_${TIMESTAMP}"
+LOG_DIR="terminal_log/terminal_log_${EXP_PREFIX}"
 if [ -d "$LOG_DIR" ]; then
     LOG_DIR="${LOG_DIR}_$(date +%s)"
 fi
@@ -14,7 +14,7 @@ echo "${EXP_NAME}"
 yolo detect train data=ultralytics/cfg/datasets/VisDrone.yaml \
                   model=cfg/yolo12s_cbase.yaml \
                   epochs=150 batch=16 imgsz=640 device=0 save=True \
-                  name="${EXP_PREFIX}/${EXP_NAME}" pretrained=yolov8m.pt \
+                  name="${EXP_PREFIX}/${EXP_NAME}" \
                   > "$LOG_DIR/${EXP_NAME}.log" 2>&1
 
 EXP_NAME="vis_v12s_bb_cbcb"
@@ -22,7 +22,15 @@ echo "${EXP_NAME}"
 yolo detect train data=ultralytics/cfg/datasets/VisDrone.yaml \
                   model=cfg/yolo12s_cbcb.yaml \
                   epochs=150 batch=16 imgsz=640 device=0 save=True \
-                  name="${EXP_PREFIX}/${EXP_NAME}" pretrained=yolov8m.pt \
+                  name="${EXP_PREFIX}/${EXP_NAME}" \
+                  > "$LOG_DIR/${EXP_NAME}.log" 2>&1
+
+EXP_NAME="vis_v12s_bb_cbb_c"
+echo "${EXP_NAME}"
+yolo detect train data=ultralytics/cfg/datasets/VisDrone.yaml \
+                  model=cfg/yolo12s_cbb_c.yaml \
+                  epochs=150 batch=16 imgsz=640 device=0 save=True \
+                  name="${EXP_PREFIX}/${EXP_NAME}" \
                   > "$LOG_DIR/${EXP_NAME}.log" 2>&1
 
 
